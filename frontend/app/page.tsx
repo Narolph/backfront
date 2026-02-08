715ca86a57f87418d05f661cd2fea281130b263b
Florian <florian.pabich@gmail.com>
2026-02-08 16:25:46 +0100

    start api

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>test
-      
-    </div>
+    <button className="btn btn-small">
+      test
+    </button>
   );
 }
