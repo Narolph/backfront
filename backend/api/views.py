827a03c8cab4ff5ab61885b807d2f38c859e76f6
Florian <florian.pabich@gmail.com>
2026-02-08 15:49:30 +0100

    second

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .serializers import TransactionsSerializer
+from .models import Transaction
 
 # Create your views here.
+class TransactionListCreateView(generics.ListCreateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionsSerializer
+
+class TransactionRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionsSerializer
+    lookup_field = 'id'
